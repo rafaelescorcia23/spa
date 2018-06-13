cd1a56e822754f0008b3c4a2aca317c0ea14f1e5
Rafael Escorcia <rafaelescorcia23@gmail.com>
2018-06-12 22:58:53 -0500

    cuarto commit seccion 4 clase 48 del curso comenzamos app_routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
+
+import {app_routing} from './app.routes';
+
+// Componentes
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ComponentsComponent } from './components/components.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    ComponentsComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { RouterModule , Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+
+
+
+const APP_ROUTES: Routes = [
+
+    {path: 'home' , component : HomeComponent },
+    {path: '**' , pathMatch: 'full', redirectTo: 'home'}
+
+]; 
+
+export const app_routing = RouterModule.forRoot(APP_ROUTES);
